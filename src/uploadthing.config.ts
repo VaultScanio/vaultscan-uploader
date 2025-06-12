a32b5576a7f015f7ff7de30d9e1c6c0426c7f86d
VaultScanio <bertramrks@gmail.com>
2025-06-12 17:23:41 +0200

    Update uploadthing.config.ts

--- src/uploadthing.config.ts
+++ src/uploadthing.config.ts
@@ -1,5 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
+console.log("📦 UPLOADTHING_SECRET:", process.env.UPLOADTHING_SECRET);
+
 const f = createUploadthing();
 
 export const ourFileRouter = {
